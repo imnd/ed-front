c194de3ba9427de62705d84bc9393f8c7f67fbb1
Konstantin Kolesnikov <kosstanley@gmail.com>
2021-07-21 19:44:06 +0300

    Tune up prettier config a bit

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,20 +2,29 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   env: {
-    apiUrl: process.env.EDVISOR_API_URL || 'http://localhost:3000'
+    baseApiUrl: process.env.EDVISOR_API_URL || 'http://localhost',
+    redirectUrl:
+      (process.env.VUE_APP_BASE_URL || 'http://localhost/') + 'redirect?link=',
   },
   head: {
     title: 'edvisor',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -30,7 +39,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    '@nuxt/typescript-build',
+    '@nuxtjs/dotenv',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -41,19 +51,26 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    hotMiddleware: {
+      client: {
+        // turn off client overlay when errors are present
+        overlay: false,
+      },
+    },
+  },
 
   styleResources: {
-    scss: ['~/assets/scss/variables.scss']
+    scss: ['~/assets/scss/variables.scss'],
   },
 
   googleFonts: {
     families: {
       Raleway: {
-        wght: [400, 500, 600, 700, 800]
-      }
+        wght: [400, 500, 600, 700, 800],
+      },
     },
     display: 'swap',
-    download: true
-  }
-};
+    download: true,
+  },
+}
